a0f30fdb9a097fa7a4a527d12341b3532a0d0ae2
Murathan Bakır <60814772+murathan519@users.noreply.github.com>
2025-03-14 16:44:18 +0300

    MoonRotation.py

---
+++ Blender/MoonRotation.py
@@ -0,0 +1,18 @@
+import bpy
+import math
+
+obj = bpy.data.objects.get("Moon")
+
+if obj:
+    start_frame = 1
+    end_frame = 12000
+    rotation_speed = 360 / (end_frame - start_frame)
+
+    for frame in range(start_frame, end_frame + 1):
+        obj.rotation_euler[2] = math.radians(rotation_speed * (frame - start_frame))
+        obj.keyframe_insert(data_path="rotation_euler", index=2, frame=frame)
+
+    bpy.context.scene.frame_start = start_frame
+    bpy.context.scene.frame_end = end_frame
+else:
+    print("L'objet 'Moon' est introuvable.")
